dd3a6a598002765f13204ea09e43db9e8efc2f89
nalarconl <nicolas.alarcon.l@usach.cl>
2021-06-07 10:42:47 +0100

    setup

---
+++ .~setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mypackage',
+  packages = ['mypackage'], # this must be the same as the name above
+  version = '0.1',
+  description = 'deconvolution of interferometric images',
+  author = 'Nicolas Alarcon',
+  author_email = 'nicolas.alarcon.l@usach.cl',
+  url = 'https://github.com/nicoasalarconl/scripts', # use the URL to the github repo
+  download_url = 'https://github.com/nicolasalarconl/scripts/tarball/0.1',
+  keywords = ['convolution', 'interferometric', 'images'],
+  classifiers = [],
+)
